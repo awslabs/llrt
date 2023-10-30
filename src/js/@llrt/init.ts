8bf144753d2e32d352c2885797fec6e7a2b72819
Davison <ridaviso@amazon.com>
2023-10-30 20:09:41 +0100

    Remove init connections already in progress

--- src/js/@llrt/init.ts
+++ src/js/@llrt/init.ts
@@ -9,9 +9,16 @@ __bootstrap.invokeHandler = async (event: any) =>
 
 const REGION = process.env.AWS_REGION || "us-east-1";
 
+const INITED = new Set<string>();
+
 __bootstrap.addAwsSdkInitTask = (service: string) => {
+  const prefix = `${service}.${REGION}`;
+  if (INITED.has(prefix)) {
+    return;
+  }
+  INITED.add(prefix);
   const start = Date.now();
-  const connectTask = fetch(`https://${service}.${REGION}.amazonaws.com`, {
+  const connectTask = fetch(`https://${prefix}.amazonaws.com`, {
     method: "GET",
   }).then(() => {
     console.log("INIT_CONNECTION", service, `${Date.now() - start}ms`);
