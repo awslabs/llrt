ab97528fc4edd7e3d5c7f886a1a7dd7e02fa7bc8
Davison <ridaviso@amazon.com>
2023-11-08 13:48:27 +0100

    Use ddb handler again

--- index.mjs
+++ index.mjs
@@ -1,58 +1,21 @@
-const iterations = 10 * 1000 * 1000;
-let count = 0;
-for (let i = 0; i < iterations; i++) {
-  setTimeout(() => {
-    count++;
-    if (i === iterations - 1) {
-      console.log(count);
-    }
-  }, 0);
-}
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
-// // let interval = setInterval(() => {
-// //   console.log("==============BANAN======");
-// //   clearInterval(interval);
-// // }, 1000);
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
-// async function main() {
-//   const start = Date.now();
-//   let count = 1;
-//   await new Promise((resolve) => {
-//     let interval = setInterval(() => {
-//       if (count == 2) {
-//         clearInterval(interval);
-//         return;
-//       }
-//       count++;
-//     }, 5);
-//     setTimeout(resolve, 20);
-//   });
-//   const end = Date.now();
-//   console.log("123");
-
-//   // await new Promise((resolve, reject) => {
-//   //   setTimeout(() => {
-//   //     console.log("========= 1");
-//   //     resolve();
-//   //   }, 10);
-//   // });
-//   // console.log("continue 1");
-//   // await new Promise((resolve, reject) => {
-//   //   setTimeout(() => {
-//   //     console.log("======== 2");
-//   //     resolve();
-//   //   }, 10);
-//   // });
-//   // console.log("continue 2");
-//   // setTimeout(() => {
-//   //   console.log("======= 3");
-//   // }, 10);
-// }
-
-// main().catch(console.error);
-
-// // setTimeout(() => {
-// //   setTimeout(() => {
-// //     console.log("2");
-// //   }, 2000);
-// // }, 1000);
+export const handler = async (event) => {
+  await docClient.send(
+    new PutCommand({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        id: Math.random().toString(36).substring(2),
+        content: JSON.stringify(event),
+      },
+    })
+  );
+  return {
+    statusCode: 200,
+    body: "OK",
+  };
+};
