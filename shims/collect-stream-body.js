3fcb9c9363da2b43f7028682fe971e9349c10080
Richard Davison <ridaviso@amazon.com>
2024-03-04 16:48:38 +0100

    Remove response.body and refactor

--- shims/collect-stream-body.js
+++ shims/collect-stream-body.js
@@ -1,3 +1,7 @@
 import { Uint8ArrayBlobAdapter } from "@smithy/util-stream";
 export const collectBody = async (streamBody) =>
-  Uint8ArrayBlobAdapter.mutate(await streamBody.typedArray());
+  Uint8ArrayBlobAdapter.mutate(
+    streamBody instanceof Uint8Array
+      ? streamBody
+      : new Uint8Array(await streamBody.arrayBuffer())
+  );
