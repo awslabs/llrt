9e21153db5b7c6a3107462e97989b3c6a0e868c4
WATANABE Shinya <nabetti1720@gmail.com>
2024-07-16 03:11:07 +0900

    fix: revert Encoding API (#474)
    * fix: revert Encoding API
    
    * refactor
    
    * Minimal UTF-16LE support
    
    * ignoreBOM support
    
    * UTF-8 BOM support
    
    * use array_chunks()
    
    * fatal error support
    
    * newline addition
    
    * TextEncoder.encodeInto() support
    
    * fix
    
    * refactor
    
    * refactor again
    
    * fixed an outrageous error in quoting sample code
    
    * optimization of fatal decisions
    
    * add necessary processing to the Buffer class
    
    ---------
    
    Co-authored-by: richarddavison <89518095+richarddavison@users.noreply.github.com>

--- llrt_core/src/modules/encoding/text_decoder.rs
+++ llrt_core/src/modules/encoding/text_decoder.rs
@@ -1,17 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-use rquickjs::{function::Opt, Ctx, Exception, Object, Result, Value};
+use llrt_utils::{encoding::Encoder, object::ObjectExt};
+use rquickjs::{function::Opt, Ctx, Object, Result, Value};
 
-use std::borrow::Cow;
-
-use crate::utils::{object::get_bytes, object::ObjectExt, result::ResultExt};
-use encoding_rs::Encoding;
+use crate::utils::{object::get_bytes, result::ResultExt};
 
 #[rquickjs::class]
 #[derive(rquickjs::class::Trace)]
 pub struct TextDecoder {
     #[qjs(skip_trace)]
-    encoding: String,
+    encoder: Encoder,
     fatal: bool,
     ignore_bom: bool,
 }
@@ -20,16 +18,14 @@ pub struct TextDecoder {
 impl<'js> TextDecoder {
     #[qjs(constructor)]
     pub fn new(ctx: Ctx<'js>, label: Opt<String>, options: Opt<Object<'js>>) -> Result<Self> {
-        let label = label
+        let encoding = label
             .0
             .filter(|lbl| !lbl.is_empty())
             .unwrap_or_else(|| String::from("utf-8"));
         let mut fatal = false;
         let mut ignore_bom = false;
 
-        let encoding = Encoding::for_label(label.as_bytes())
-            .map(|enc| enc.name().to_string())
-            .or_throw_msg(&ctx, "Unsupported encoding label")?;
+        let encoder = Encoder::from_str(&encoding).or_throw(&ctx)?;
 
         if let Some(options) = options.0 {
             if let Some(opt) = options.get_optional("fatal")? {
@@ -41,16 +37,15 @@ impl<'js> TextDecoder {
         }
 
         Ok(TextDecoder {
-            encoding,
+            encoder,
             fatal,
             ignore_bom,
         })
     }
 
     #[qjs(get)]
-    fn encoding(&self) -> String {
-        let s = self.encoding.clone();
-        s.replace('_', "-").to_ascii_lowercase()
+    fn encoding(&self) -> &str {
+        self.encoder.as_label()
     }
 
     #[qjs(get)]
@@ -65,25 +60,16 @@ impl<'js> TextDecoder {
 
     pub fn decode(&self, ctx: Ctx<'js>, buffer: Value<'js>) -> Result<String> {
         let bytes = get_bytes(&ctx, buffer)?;
-
-        let decoder = Encoding::for_label(self.encoding.as_bytes()).unwrap();
-
-        let str: Cow<str>;
-        let has_error: bool;
-
-        if decoder == encoding_rs::UTF_8 {
-            (str, has_error) = match self.ignore_bom {
-                false => decoder.decode_with_bom_removal(&bytes),
-                true => decoder.decode_without_bom_handling(&bytes),
-            }
+        let start_pos = if !self.ignore_bom && bytes.len() >= 2 && bytes[..2] == [0xFF, 0xFE] {
+            2
+        } else if !self.ignore_bom && bytes.len() >= 3 && bytes[..3] == [0xEF, 0xBB, 0xBF] {
+            3
         } else {
-            (str, _, has_error) = decoder.decode(&bytes);
-        }
+            0
+        };
 
-        if self.fatal && has_error {
-            return Err(Exception::throw_message(&ctx, "Fatal error"));
-        }
-
-        Ok(str.into_owned())
+        self.encoder
+            .encode_to_string(&bytes[start_pos..], !self.fatal)
+            .or_throw(&ctx)
     }
 }
