9f245c5a35e32c87ec451499215150e2e699e450
Emile Fugulin <code@efugulin.com>
2024-07-24 09:57:34 -0400

    Fix init

--- llrt_core/src/modules/events/mod.rs
+++ llrt_core/src/modules/events/mod.rs
@@ -1,6 +1,22 @@
-pub use llrt_modules::events::*;
+pub use llrt_modules::events::{
+    EmitError, Emitter, EventEmitter, EventKey, EventList, EventsModule,
+};
+use rquickjs::{Class, Ctx, Result};
 
-use self::abort_signal::AbortSignal;
+use self::{abort_controller::AbortController, abort_signal::AbortSignal};
 
 pub mod abort_controller;
 pub mod abort_signal;
+
+pub fn init(ctx: &Ctx<'_>) -> Result<()> {
+    llrt_modules::events::init(ctx)?;
+
+    let globals = ctx.globals();
+
+    Class::<AbortController>::define(&globals)?;
+    Class::<AbortSignal>::define(&globals)?;
+
+    AbortSignal::add_event_emitter_prototype(ctx)?;
+
+    Ok(())
+}
