6770f3cce4e52f485285fdf6ddb3d6677c232c12
Emile Fugulin <code@efugulin.com>
2024-07-25 18:29:10 -0400

    More licenses

--- llrt_core/src/modules/events/mod.rs
+++ llrt_core/src/modules/events/mod.rs
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 pub use llrt_modules::events::{
     EmitError, Emitter, EventEmitter, EventKey, EventList, EventsModule,
 };
