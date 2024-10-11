a44cf0ec4cfe7e39ba897c131a2dba98d2509cc6
WATANABE Shinya <nabetti1720@gmail.com>
2024-10-11 17:43:47 +0900

    fix: Load error when no path is specified

---
+++ hello.js
@@ -0,0 +1,2 @@
+export const hello = "hello world!";
+console.log(hello);
--- tests/unit/import.test.ts
+++ tests/unit/import.test.ts
@@ -1,14 +1,38 @@
 const CWD = process.cwd();
 
 describe("import", () => {
-  it("should import a js file", async () => {
+  it("should import a js file (absolute path)", async () => {
     const mod = await import(`${CWD}/fixtures/hello.js`);
 
     expect(mod.hello).toEqual("hello world!");
   });
 
-  it("should import a json file", async () => {
+  it("should import a json file (absolute path)", async () => {
     const mod = await import(`${CWD}/package.json`);
+
+    expect(mod.default.private).toEqual(true);
+  });
+
+  it("should import a js file (relative path)", async () => {
+    const mod = await import("../../hello.js");
+
+    expect(mod.hello).toEqual("hello world!");
+  });
+
+  it("should import a json file (relative path)", async () => {
+    const mod = await import("../../package.json");
+
+    expect(mod.default.private).toEqual(true);
+  });
+
+  it("should import a js file (path unspecified)", async () => {
+    const mod = await import("hello.js");
+
+    expect(mod.hello).toEqual("hello world!");
+  });
+
+  it("should import a json file (path unspecified)", async () => {
+    const mod = await import("package.json");
 
     expect(mod.default.private).toEqual(true);
   });
