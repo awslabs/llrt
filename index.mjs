f48461bfdfbb2cd8ade43b0c65ddeada6d1d9504
richarddavison <89518095+richarddavison@users.noreply.github.com>
2024-10-15 09:26:32 +0200

    Revert index.mjs

--- index.mjs
+++ index.mjs
@@ -1,1 +1,21 @@
-console.log("\x1B[32m✔\x1B[0m", "a");
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const handler = async (event) => {
+  await docClient.send(
+    new PutCommand({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        id: Math.random().toString(36).substring(2),
+        content: JSON.stringify(event),
+      },
+    })
+  );
+  return {
+    statusCode: 200,
+    body: "OK",
+  };
+};
