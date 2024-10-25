d08a1c47b616be73d2ddb386888c876d4cc2845a
Emile Fugulin <code@efugulin.com>
2024-10-24 22:34:48 -0400

    Make it more readable

--- modules/llrt_http/src/compression.rs
+++ modules/llrt_http/src/compression.rs
@@ -1,5 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
+#[cfg(not(any(feature = "flate2-c", feature = "flate2-rust")))]
+compile_error!("Either the `flate2-c` or `flate2-rust` feature must be enabled");
+
+#[cfg(not(any(feature = "brotli-c", feature = "brotli-rust")))]
+compile_error!("Either the `brotli-c` or `brotli-rust` feature must be enabled");
+
 pub(crate) mod zstd {
     use std::io::{BufReader, Read};
 
@@ -11,61 +18,46 @@ pub(crate) mod zstd {
     }
 }
 
+#[cfg(any(feature = "flate2-c", feature = "flate2-rust"))]
 pub(crate) mod gz {
     use std::io::Read;
 
     use flate2::read::GzDecoder;
 
     pub fn decoder<R: Read>(r: R) -> GzDecoder<R> {
-        #[cfg(any(feature = "flate2-c", feature = "flate2-rust"))]
-        {
-            GzDecoder::new(r)
-        }
-        #[cfg(not(any(feature = "flate2-c", feature = "flate2-rust")))]
-        {
-            compile_error!("Either the `flate2-c` or `flate2-rust` feature must be enabled")
-        }
+        GzDecoder::new(r)
     }
 }
 
+#[cfg(any(feature = "flate2-c", feature = "flate2-rust"))]
 pub(crate) mod zlib {
     use std::io::Read;
 
     use flate2::read::ZlibDecoder;
 
     pub fn decoder<R: Read>(r: R) -> ZlibDecoder<R> {
-        #[cfg(any(feature = "flate2-c", feature = "flate2-rust"))]
-        {
-            ZlibDecoder::new(r)
-        }
-        #[cfg(not(any(feature = "flate2-c", feature = "flate2-rust")))]
-        {
-            compile_error!("Either the `flate2-c` or `flate2-rust` feature must be enabled")
-        }
+        ZlibDecoder::new(r)
     }
 }
 
+#[cfg(feature = "brotli-c")]
 pub(crate) mod brotli {
-    #[cfg(feature = "brotli-c")]
     use std::io::BufRead;
-    #[cfg(all(not(feature = "brotli-c"), feature = "brotli-rust"))]
-    use std::io::Read;
 
-    #[cfg(all(not(feature = "brotli-c"), feature = "brotli-rust"))]
-    use brotli::Decompressor as BrotliDecoder;
-    #[cfg(feature = "brotli-c")]
     use brotlic::DecompressorReader as BrotliDecoder;
 
-    #[cfg(feature = "brotli-c")]
     pub fn decoder<R: BufRead>(r: R) -> BrotliDecoder<R> {
         BrotliDecoder::new(r)
     }
+}
 
-    #[cfg(all(not(feature = "brotli-c"), feature = "brotli-rust"))]
+#[cfg(all(not(feature = "brotli-c"), feature = "brotli-rust"))]
+pub(crate) mod brotli {
+    use std::io::Read;
+
+    use brotli::Decompressor as BrotliDecoder;
+
     pub fn decoder<R: Read>(r: R) -> BrotliDecoder<R> {
         BrotliDecoder::new(r, 8_096)
     }
-
-    #[cfg(not(any(feature = "brotli-c", feature = "brotli-rust")))]
-    compile_error!("Either the `brotli-c` or `brotli-rust` feature must be enabled");
 }
