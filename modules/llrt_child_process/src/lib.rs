c55a16c06a8c54da9d55d91cd807c987575a5a04
Richard Davison <ridaviso@amazon.com>
2024-11-14 08:54:00 +0100

    Temporary disable spawn_tests in crate

--- modules/llrt_child_process/src/lib.rs
+++ modules/llrt_child_process/src/lib.rs
@@ -635,90 +635,91 @@ impl From<ChildProcessModule> for ModuleInfo<ChildProcessModule> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use llrt_buffer as buffer;
-    use llrt_test::{test_async_with, ModuleEvaluator};
+// FIXME: add back once https://github.com/DelSkayn/rquickjs/pull/385 has landed
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use llrt_buffer as buffer;
+//     use llrt_test::{test_async_with, ModuleEvaluator};
 
-    #[tokio::test]
-    async fn test_spawn() {
-        test_async_with(|ctx| {
-            Box::pin(async move {
-                buffer::init(&ctx).unwrap();
+//     #[tokio::test]
+//     async fn test_spawn() {
+//         test_async_with(|ctx| {
+//             Box::pin(async move {
+//                 buffer::init(&ctx).unwrap();
 
-                ModuleEvaluator::eval_rust::<ChildProcessModule>(ctx.clone(), "child_process")
-                    .await
-                    .unwrap();
+//                 ModuleEvaluator::eval_rust::<ChildProcessModule>(ctx.clone(), "child_process")
+//                     .await
+//                     .unwrap();
 
-                let message: String = ModuleEvaluator::eval_js(
-                    ctx,
-                    "test",
-                    r#"
-                   import {spawn} from "child_process";
+//                 let message: String = ModuleEvaluator::eval_js(
+//                     ctx,
+//                     "test",
+//                     r#"
+//                    import {spawn} from "child_process";
 
-                    let resolve = null;
-                    const deferred = new Promise(res => {
-                        resolve = res;
-                    });
+//                     let resolve = null;
+//                     const deferred = new Promise(res => {
+//                         resolve = res;
+//                     });
 
-                    spawn("echo", ["hello"]).stdout.on("data", (data) => {
-                        resolve(data.toString().trim())
-                    });
+//                     spawn("echo", ["hello"]).stdout.on("data", (data) => {
+//                         resolve(data.toString().trim())
+//                     });
 
-                    export default await deferred;
-                
-                "#,
-                )
-                .await
-                .unwrap()
-                .get("default")
-                .unwrap();
+//                     export default await deferred;
 
-                assert_eq!(message, "hello");
-            })
-        })
-        .await;
-    }
+//                 "#,
+//                 )
+//                 .await
+//                 .unwrap()
+//                 .get("default")
+//                 .unwrap();
 
-    #[tokio::test]
-    async fn test_spawn_shell() {
-        test_async_with(|ctx| {
-            Box::pin(async move {
-                buffer::init(&ctx).unwrap();
+//                 assert_eq!(message, "hello");
+//             })
+//         })
+//         .await;
+//     }
 
-                ModuleEvaluator::eval_rust::<ChildProcessModule>(ctx.clone(), "child_process")
-                    .await
-                    .unwrap();
+//     #[tokio::test]
+//     async fn test_spawn_shell() {
+//         test_async_with(|ctx| {
+//             Box::pin(async move {
+//                 buffer::init(&ctx).unwrap();
 
-                let message: String = ModuleEvaluator::eval_js(
-                    ctx,
-                    "test",
-                    r#"
-                    import {spawn} from "child_process";
+//                 ModuleEvaluator::eval_rust::<ChildProcessModule>(ctx.clone(), "child_process")
+//                     .await
+//                     .unwrap();
 
-                    let resolve = null;
-                    const deferred = new Promise(res => {
-                        resolve = res;
-                    });
+//                 let message: String = ModuleEvaluator::eval_js(
+//                     ctx,
+//                     "test",
+//                     r#"
+//                     import {spawn} from "child_process";
 
-                    spawn("echo", ["hello"], {
-                        shell: true
-                    }).stdout.on("data", (data) => {
-                        resolve(data.toString().trim())
-                    });
+//                     let resolve = null;
+//                     const deferred = new Promise(res => {
+//                         resolve = res;
+//                     });
 
-                    export default await deferred;
-                "#,
-                )
-                .await
-                .unwrap()
-                .get("default")
-                .unwrap();
+//                     spawn("echo", ["hello"], {
+//                         shell: true
+//                     }).stdout.on("data", (data) => {
+//                         resolve(data.toString().trim())
+//                     });
 
-                assert_eq!(message, "hello");
-            })
-        })
-        .await;
-    }
-}
+//                     export default await deferred;
+//                 "#,
+//                 )
+//                 .await
+//                 .unwrap()
+//                 .get("default")
+//                 .unwrap();
+
+//                 assert_eq!(message, "hello");
+//             })
+//         })
+//         .await;
+//     }
+// }
