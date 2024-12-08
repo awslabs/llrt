84b649206a58f4f6e0d93f05f3fa2b7d8a0645dc
Richard Davison <ridaviso@amazon.com>
2024-12-08 21:26:09 +0100

    Fix console log level filtering and add better json parse error message

--- libs/llrt_json/src/parse.rs
+++ libs/llrt_json/src/parse.rs
@@ -1,13 +1,40 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use llrt_utils::result::ResultExt;
-use rquickjs::{Array, Ctx, IntoJs, Null, Object, Result, Undefined, Value};
+use llrt_utils::bytes::ObjectBytes;
+use rquickjs::{Array, Ctx, Exception, IntoJs, Null, Object, Result, Undefined, Value};
 use simd_json::{Node, StaticNode};
+
+pub fn json_parse_string<'js>(ctx: Ctx<'js>, bytes: ObjectBytes<'js>) -> Result<Value<'js>> {
+    let bytes = bytes.as_bytes();
+    json_parse(&ctx, bytes)
+}
 
 pub fn json_parse<'js, T: Into<Vec<u8>>>(ctx: &Ctx<'js>, json: T) -> Result<Value<'js>> {
     let mut json: Vec<u8> = json.into();
-    let tape = simd_json::to_tape(&mut json).or_throw(ctx)?;
+    let tape = match simd_json::to_tape(&mut json) {
+        Ok(tape) => tape,
+        Err(err) => {
+            let mut itoa = itoa::Buffer::new();
+            let mut error_msg = String::with_capacity(256);
+            let json_length = json.len();
+            if json_length < 128 {
+                error_msg.reserve(json_length);
+                error_msg.push('\"');
+                error_msg.push_str(&std::string::String::from_utf8_lossy(&mut json));
+                error_msg.push_str("\" ");
+            }
+
+            error_msg.push_str("not valid JSON at index ");
+            error_msg.push_str(itoa.format(err.index()));
+            if let Some(char) = err.character() {
+                error_msg.push_str(" ('");
+                error_msg.push(char);
+                error_msg.push_str("')");
+            }
+            return Err(Exception::throw_syntax(ctx, &error_msg));
+        },
+    };
     let tape = tape.0;
 
     if let Some(first) = tape.first() {
