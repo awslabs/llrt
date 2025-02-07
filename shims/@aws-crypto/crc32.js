29348813adb05371c5dd9b3f2f41b018dc629e07
WATANABE Shinya <nabetti1720@gmail.com>
2025-02-08 08:23:27 +0900

    fix(shims): fix @aws-crypto shims not loading

--- shims/@aws-crypto/crc32.js
+++ shims/@aws-crypto/crc32.js
@@ -1,5 +1,4 @@
 import { Crc32 as CryptoCrc32 } from "crypto";
-import { isEmptyData, numToUint8 } from "@aws-crypto/util";
 
 export const Crc32 = CryptoCrc32;
 
@@ -18,4 +17,20 @@ export class AwsCrc32 {
   reset() {
     this.#crc32 = new CryptoCrc32();
   }
+}
+
+function isEmptyData(data) {
+  if (typeof data === "string") {
+    return data.length === 0;
+  }
+  return data.byteLength === 0;
+}
+
+function numToUint8(num) {
+  return new Uint8Array([
+    (num & 0xff000000) >> 24,
+    (num & 0x00ff0000) >> 16,
+    (num & 0x0000ff00) >> 8,
+    num & 0x000000ff,
+  ]);
 }
