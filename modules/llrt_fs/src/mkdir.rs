824577c0e64ed44d7d71f6046860e2e73ca54ec8
richarddavison <89518095+richarddavison@users.noreply.github.com>
2025-02-18 10:39:22 +0100

    fix: mkdir path resolve (#841)
    * Fix mkdir path resolve
    
    * remove unused import
    
    * Clippy

--- modules/llrt_fs/src/mkdir.rs
+++ modules/llrt_fs/src/mkdir.rs
@@ -1,13 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 use crate::chmod::{set_mode, set_mode_sync};
+
+use llrt_path::resolve_path;
 use llrt_utils::result::ResultExt;
 use ring::rand::{SecureRandom, SystemRandom};
 use rquickjs::{function::Opt, Ctx, Object, Result};
 use tokio::fs;
 
 pub async fn mkdir<'js>(ctx: Ctx<'js>, path: String, options: Opt<Object<'js>>) -> Result<String> {
-    let (recursive, mode) = get_params(options);
+    let (recursive, mode, path) = get_params(&path, options)?;
 
     if recursive {
         fs::create_dir_all(&path).await
@@ -22,7 +24,7 @@ pub async fn mkdir<'js>(ctx: Ctx<'js>, path: String, options: Opt<Object<'js>>) 
 }
 
 pub fn mkdir_sync<'js>(ctx: Ctx<'js>, path: String, options: Opt<Object<'js>>) -> Result<String> {
-    let (recursive, mode) = get_params(options);
+    let (recursive, mode, path) = get_params(&path, options)?;
 
     if recursive {
         std::fs::create_dir_all(&path)
@@ -36,7 +38,7 @@ pub fn mkdir_sync<'js>(ctx: Ctx<'js>, path: String, options: Opt<Object<'js>>) -
     Ok(path)
 }
 
-fn get_params(options: Opt<Object>) -> (bool, u32) {
+fn get_params(path: &str, options: Opt<Object>) -> Result<(bool, u32, String)> {
     let mut recursive = false;
     let mut mode = 0o777;
 
@@ -44,7 +46,8 @@ fn get_params(options: Opt<Object>) -> (bool, u32) {
         recursive = options.get("recursive").unwrap_or_default();
         mode = options.get("mode").unwrap_or(0o777);
     }
-    (recursive, mode)
+    let path = resolve_path([path])?;
+    Ok((recursive, mode, path))
 }
 
 const CHARS: &[u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
