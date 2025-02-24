48b407be0ddf13319b8d5716b7116816d20db8c9
WATANABE Shinya <nabetti1720@gmail.com>
2025-02-24 21:59:50 +0900

    add error test

--- tests/unit/dns.test.ts
+++ tests/unit/dns.test.ts
@@ -1,27 +1,54 @@
 const dns = require("dns");
 
 describe("lookup", () => {
-  it("optionless", (done) => {
+  it("localhost name resolution should be possible (optionless)", (done) => {
     dns.lookup("localhost", (err, address, family) => {
+      expect(err).toBeNull();
       expect(address === "::1" || address === "127.0.0.1").toBeTruthy();
       expect(family === 4 || family === 6).toBeTruthy();
       done();
     });
   });
 
-  it("option - integer", (done) => {
+  it("Name resolution for localhost2 should result in an error (optionless)", () => {
+    dns.lookup("localhost2", (err, address, family) => {
+      expect(err.message).toEqual(
+        "failed to lookup address information: nodename nor servname provided, or not known"
+      );
+    });
+  });
+
+  it("localhost name resolution should be possible (integer option)", (done) => {
     dns.lookup("localhost", 4, (err, address, family) => {
+      expect(err).toBeNull();
       expect(address).toEqual("127.0.0.1");
       expect(family).toEqual(4);
       done();
     });
   });
 
-  it("option - record", (done) => {
+  it("Name resolution for localhost2 should result in an error (integer option)", () => {
+    dns.lookup("localhost2", 4, (err, address, family) => {
+      expect(err.message).toEqual(
+        "failed to lookup address information: nodename nor servname provided, or not known"
+      );
+    });
+  });
+
+  it("localhost name resolution should be possible (record option)", (done) => {
     dns.lookup("localhost", { family: 4 }, (err, address, family) => {
+      expect(err).toBeNull();
       expect(address).toEqual("127.0.0.1");
       expect(family).toEqual(4);
       done();
+    });
+  });
+
+  it("Name resolution for localhost2 should result in an error (record option)", () => {
+    dns.lookup("localhost2", { family: 4 }, (err, address, family) => {
+      expect(err.message).toEqual(
+        "failed to lookup address information: nodename nor servname provided, or not known"
+      );
     });
   });
 });
