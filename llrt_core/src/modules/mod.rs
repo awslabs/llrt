2c3f2eb0f1d4bed7c22e3c8d931ef07ebe08599c
Shinya Watanabe <nabetti1720@gmail.com>
2025-04-05 16:20:43 +0900

    refactor(modules): give llrt_modules the responsibility for assembling modules

--- llrt_core/src/modules/mod.rs
+++ llrt_core/src/modules/mod.rs
@@ -3,8 +3,9 @@
 #[cfg(not(feature = "lambda"))]
 pub use llrt_modules::console;
 pub use llrt_modules::{
-    abort, assert, buffer, child_process, crypto, dns, events, exceptions, fs, http, navigator,
-    net, os, path, perf_hooks, process, stream_web, string_decoder, timers, tty, url, util, zlib,
+    abort, assert, buffer, child_process, crypto, dns, events, exceptions, fs, http,
+    module_builder, navigator, net, os, path, perf_hooks, process, stream_web, string_decoder,
+    timers, tty, url, util, zlib,
 };
 
 #[cfg(feature = "lambda")]
