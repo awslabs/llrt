73211138bf8a95f144d381244fe64dc4dfcf058b
Shinya Watanabe <nabetti1720@gmail.com>
2025-04-30 10:27:15 +0900

    support for asynchronous hooks in dns.lookup()

--- modules/llrt_dns/src/lib.rs
+++ modules/llrt_dns/src/lib.rs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 use llrt_context::CtxExtension;
 use llrt_dns_cache::lookup_host;
+#[cfg(feature = "hooking")]
+use llrt_hooking::{invoke_async_hook, register_finalization_registry, HookType, ProviderType};
 use llrt_utils::{
     module::{export_default, ModuleInfo},
     result::ResultExt,
@@ -19,14 +21,30 @@ fn lookup<'js>(ctx: Ctx<'js>, hostname: String, args: Rest<Value<'js>>) -> Resul
         .and_then(|v| v.into_function())
         .or_throw_msg(&ctx, "Callback parameter is not a function")?;
 
+    // SAFETY: Since it checks in advance whether it is an Function type, we can always get a pointer to the Function.
+    let _uid = unsafe { cb.as_raw().u.ptr } as usize;
+    #[cfg(feature = "hooking")]
+    {
+        register_finalization_registry(&ctx, cb.clone().into_value(), _uid)?;
+        invoke_async_hook(&ctx, HookType::Init, ProviderType::GetAddrInfoReqWrap, _uid)?;
+    }
+
     ctx.clone().spawn_exit(async move {
         match lookup_host(&hostname, args_iter.next()).await {
             Ok((address, family)) => {
+                #[cfg(feature = "hooking")]
+                invoke_async_hook(&ctx, HookType::Before, ProviderType::None, _uid)?;
                 () = cb.call((Null.into_js(&ctx), address, family))?;
+                #[cfg(feature = "hooking")]
+                invoke_async_hook(&ctx, HookType::After, ProviderType::None, _uid)?;
                 Ok::<_, Error>(())
             },
             Err(err) => {
-                () = cb.call((Exception::from_message(ctx, &err.to_string()),))?;
+                #[cfg(feature = "hooking")]
+                invoke_async_hook(&ctx, HookType::Before, ProviderType::None, _uid)?;
+                () = cb.call((Exception::from_message(ctx.clone(), &err.to_string()),))?;
+                #[cfg(feature = "hooking")]
+                invoke_async_hook(&ctx, HookType::After, ProviderType::None, _uid)?;
                 Ok(())
             },
         }
