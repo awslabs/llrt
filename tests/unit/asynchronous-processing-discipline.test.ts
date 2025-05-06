aa068d9ccef0a4272478ca3e67ff61cc90a4b16a
Shinya Watanabe <nabetti1720@gmail.com>
2025-05-06 19:22:33 +0900

    modify test

--- tests/unit/asynchronous-processing-discipline.test.ts
+++ tests/unit/asynchronous-processing-discipline.test.ts
@@ -1,19 +1,22 @@
-describe("Asynchronous Processing Discipline", () => {
-  function expectSequentialNumbers(tracked) {
-    expect(tracked).toEqual(
-      Array.from({ length: tracked.length }, (_, i) => i + 1)
-    );
-  }
+async function waitAndExpectSequentialNumbers(tracked, delayMs) {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      expect(tracked).toEqual(
+        Array.from({ length: tracked.length }, (_, i) => i + 1)
+      );
+      resolve();
+    }, delayMs);
+  });
+}
 
+describe("Execution order of synchronous and asynchronous processes", () => {
   it("Synchronous operations have priority over microtasks.", async () => {
     const tracked = [];
 
     queueMicrotask(() => tracked.push(2));
     tracked.push(1);
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
 
   it("Synchronous operations have priority over macrotasks.", async () => {
@@ -22,9 +25,7 @@ describe("Asynchronous Processing Discipline", () => {
     setTimeout(() => tracked.push(2));
     tracked.push(1);
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
 
   it("Microtasks are executed in the order they are registered.", async () => {
@@ -33,9 +34,7 @@ describe("Asynchronous Processing Discipline", () => {
     Promise.resolve().then(() => tracked.push(1));
     queueMicrotask(() => tracked.push(2));
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
 
   it("When a microtask occurs within a microtask, it is placed at the end of the accumulated microtasks.", async () => {
@@ -43,13 +42,11 @@ describe("Asynchronous Processing Discipline", () => {
 
     Promise.resolve().then(() => {
       tracked.push(1);
-      queueMicrotask(() => (tracked.push(3)));
+      queueMicrotask(() => tracked.push(3));
     });
     queueMicrotask(() => tracked.push(2));
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
 
   it("If a macrotask and a microtask are registered at the same time, the microtask will take priority.", async () => {
@@ -59,21 +56,17 @@ describe("Asynchronous Processing Discipline", () => {
     Promise.resolve().then(() => tracked.push(1));
     queueMicrotask(() => tracked.push(2));
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
 
   it("Macro tasks with different scheduled firing times are executed in the order of their scheduled firing times.", async () => {
     const tracked = [];
 
-    setTimeout(() => tracked.push(3), 10);
+    setTimeout(() => tracked.push(3), 20);
+    setTimeout(() => tracked.push(2), 10);
     setTimeout(() => tracked.push(1));
-    setTimeout(() => tracked.push(2));
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 20);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 30);
   });
 
   it("If a microtask occurs within a macrotask, all microtasks are executed before the next macrotask is executed.", async () => {
@@ -86,9 +79,7 @@ describe("Asynchronous Processing Discipline", () => {
     });
     setTimeout(() => tracked.push(4));
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
 
   it("When a new macrotask occurs among the macrotasks, it is placed at the end of the accumulated macrotasks.", async () => {
@@ -100,11 +91,11 @@ describe("Asynchronous Processing Discipline", () => {
     });
     setTimeout(() => tracked.push(2));
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
+});
 
+describe("Asynchronous Microtask Function", () => {
   it("If an asynchronous microtask function does not contain an await, it is executed entirely at the time of function registration.", async () => {
     const tracked = [];
 
@@ -115,41 +106,34 @@ describe("Asynchronous Processing Discipline", () => {
     microTaskFunction();
     tracked.push(2);
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
-  });
-
-  it("If an asynchronous microtask function contains an asynchronous operation with await, the subsequent operation will be executed after the asynchronous operation has finished.", async () => {
-    const tracked = [];
-
-    async function microTaskFunction() {
-      await Promise.resolve();
-      tracked.push(2);
-    }
-
-    microTaskFunction();
-    tracked.push(1);
-
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
 
   it("If an asynchronous microtask function contains an asynchronous operation without await, the subsequent operation will execute immediately because the asynchronous operation will not wait for completion.", async () => {
     const tracked = [];
 
     async function microTaskFunction() {
-      Promise.resolve();
+      Promise.resolve().then(() => tracked.push(3));
       tracked.push(1);
     }
 
     microTaskFunction();
     tracked.push(2);
 
-    await new Promise((resolve) => {
-      setTimeout(() => { expectSequentialNumbers(tracked); resolve(); }, 10);
-    });
+    await waitAndExpectSequentialNumbers(tracked, 10);
   });
 
+  it("If an asynchronous microtask function contains an asynchronous operation with await, the subsequent operation will be executed after the asynchronous operation has finished.", async () => {
+    const tracked = [];
+
+    async function microTaskFunction() {
+      await Promise.resolve().then(() => tracked.push(2));
+      tracked.push(3);
+    }
+
+    microTaskFunction();
+    tracked.push(1);
+
+    await waitAndExpectSequentialNumbers(tracked, 10);
+  });
 });
