1ef60dd9df733d406986947346e4569d44ecbecd
Shinya Watanabe <nabetti1720@gmail.com>
2025-08-08 22:45:34 +0900

    feat(wpt): expand the scope of wpt testing

--- tests/wpt/WebCryptoAPI.derive_bits_keys.test.ts
+++ tests/wpt/WebCryptoAPI.derive_bits_keys.test.ts
@@ -6,8 +6,10 @@ import { fileURLToPath } from "url";
 const SKIP_FILES = [
   "cfrg_curves_bits_curve25519.https.any.js", // ReferenceError: define_tests_25519 is not defined
   "cfrg_curves_bits_curve448.https.any.js", // ReferenceError: define_tests_448 is not defined
+  "cfrg_curves_bits_curve448.tentative.https.any.js", // ReferenceError: define_tests_448 is not defined
   "cfrg_curves_keys_curve25519.https.any.js", // ReferenceError: define_tests_25519 is not defined
   "cfrg_curves_keys_curve448.https.any.js", // ReferenceError: define_tests_448 is not defined
+  "cfrg_curves_keys_curve448.tentative.https.any.js", // ReferenceError: define_tests_448 is not defined
   "derive_key_and_encrypt.https.any.js", // ReferenceError: define_tests is not defined
   "derived_bits_length.https.any.js", // ReferenceError: define_tests is not defined
   "ecdh_bits.https.any.js", // ReferenceError: define_tests is not defined
