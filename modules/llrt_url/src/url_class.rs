f2a7b3213f88baee067dffb2bceb7fa953454635
Shinya Watanabe <nabetti1720@gmail.com>
2025-08-23 10:55:11 +0900

    chore

--- modules/llrt_url/src/url_class.rs
+++ modules/llrt_url/src/url_class.rs
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 #![allow(clippy::uninlined_format_args)]
-
 use std::{cell::RefCell, rc::Rc};
 
 use rquickjs::{
