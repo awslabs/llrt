8578e60b7fcd250c4ccf2fc3ae833111a0c705e8
Shinya Watanabe <nabetti1720@gmail.com>
2025-08-26 04:31:57 +0900

    fix(url): convert trailing space from opaque path (#1110)
    * fix(url): convert trailing space from opaque path
    
    * chore
    
    * fix

--- modules/llrt_url/src/lib.rs
+++ modules/llrt_url/src/lib.rs
@@ -113,6 +113,25 @@ pub fn url_format<'js>(url: Class<'js, URL<'js>>, options: Opt<Value<'js>>) -> R
     Ok(string)
 }
 
+// https://url.spec.whatwg.org/#cannot-be-a-base-url-path-state
+pub fn convert_trailing_space(url: &mut Url) {
+    if matches!(
+        url.scheme(),
+        "file" | "ftp" | "http" | "https" | "ws" | "wss"
+    ) {
+        return;
+    }
+
+    let path = url.path();
+    let has_remaining = url.fragment().is_some() || url.query().is_some();
+
+    #[allow(clippy::manual_strip)]
+    if path.ends_with(' ') && has_remaining {
+        let new_path = [&path[..path.len() - 1], "%20"].concat();
+        url.set_path(&new_path);
+    }
+}
+
 pub fn init(ctx: &Ctx<'_>) -> Result<()> {
     let globals = ctx.globals();
 
