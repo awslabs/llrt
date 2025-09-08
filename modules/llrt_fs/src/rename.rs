38ae7bcd613bfb9a846d57f6736052b634f37678
Benjamin Chausse <benjamin@caido.io>
2025-09-08 09:52:10 -0400

    Add rename and rename_sync to fs module

---
+++ modules/llrt_fs/src/rename.rs
@@ -0,0 +1,19 @@
+use llrt_utils::result::ResultExt;
+use rquickjs::{Ctx, Result};
+
+pub(crate) fn rename_error(from: &str, to: &str) -> String {
+    ["Can't rename file/folder from \"", from, "\" to \"", to, "\""].concat()
+}
+
+pub async fn rename(ctx: Ctx<'_>, old_path: &str, new_path: &str) -> Result<()> {
+    tokio::fs::rename(old_path, new_path)
+        .await
+        .or_throw_msg(&ctx, &rename_error(old_path, new_path))?;
+    Ok(())
+}
+
+pub fn rename_sync(ctx: Ctx<'_>, old_path: &str, new_path: &str) -> Result<()> {
+    std::fs::rename(old_path, new_path)
+        .or_throw_msg(&ctx, &rename_error(old_path, new_path))?;
+    Ok(())
+}
