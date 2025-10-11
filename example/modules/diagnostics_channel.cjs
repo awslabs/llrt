a0301e6af3925c3325e834a8bcfc61fee8eb3a16
Shinya Watanabe <nabetti1720@gmail.com>
2025-10-11 15:38:02 +0900

    feat(core): Implement pseudo node.js module import mechanism

---
+++ example/modules/diagnostics_channel.cjs
@@ -0,0 +1,4 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+const diagnostics_channel = require("dc-polyfill");
+module.exports = { diagnostics_channel, ...diagnostics_channel };
