b9d44e824a5c2265e94749ab9aa42d71c55d14c7
Shinya Watanabe <nabetti1720@gmail.com>
2025-11-29 15:19:42 +0900

    feat(perf_hooks): Improve performance interface compatibility (#1256)

--- modules/llrt_perf_hooks/src/lib.rs
+++ modules/llrt_perf_hooks/src/lib.rs
@@ -1,59 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-use llrt_utils::{
-    module::{export_default, ModuleInfo},
-    time,
-};
+use llrt_events::Emitter;
+use llrt_utils::module::{export_default, ModuleInfo};
 use rquickjs::{
-    atom::PredefinedAtom,
     module::{Declarations, Exports, ModuleDef},
-    prelude::Func,
-    Ctx, JsLifetime, Object, Result,
+    Class, Ctx, Object, Result,
 };
 
-fn get_time_origin() -> f64 {
-    let time_origin = time::origin_nanos() as f64;
+use crate::performance::Performance;
 
-    time_origin / 1e6
-}
-
-fn now() -> f64 {
-    let now = time::now_nanos();
-    let started = time::origin_nanos();
-    let elapsed = now.checked_sub(started).unwrap_or_default();
-
-    (elapsed as f64) / 1e6
-}
-
-fn to_json(ctx: Ctx<'_>) -> Result<Object<'_>> {
-    let obj = Object::new(ctx.clone())?;
-
-    obj.set("timeOrigin", get_time_origin())?;
-
-    Ok(obj)
-}
-
-struct PerfInitedUserData;
-
-unsafe impl JsLifetime<'_> for PerfInitedUserData {
-    type Changed<'to> = PerfInitedUserData;
-}
-
-fn new_performance(ctx: Ctx<'_>) -> Result<Object<'_>> {
-    let global = ctx.globals();
-
-    let inited = ctx.userdata::<PerfInitedUserData>().is_some();
-    if !inited {
-        ctx.store_userdata(PerfInitedUserData)?;
-        let performance = Object::new(ctx)?;
-        performance.set("timeOrigin", get_time_origin())?;
-        performance.set("now", Func::from(now))?;
-        performance.set(PredefinedAtom::ToJSON, Func::from(to_json))?;
-        global.set("performance", performance.clone())?;
-        return Ok(performance);
-    }
-    global.get("performance")
-}
+mod performance;
 
 pub struct PerfHooksModule;
 
@@ -66,8 +22,11 @@ impl ModuleDef for PerfHooksModule {
 
     fn evaluate<'js>(ctx: &Ctx<'js>, exports: &Exports<'js>) -> Result<()> {
         export_default(ctx, exports, |default| {
-            let performance = new_performance(ctx.clone())?;
+            let globals = ctx.globals();
+
+            let performance: Object = globals.get("performance")?;
             default.set("performance", performance)?;
+
             Ok(())
         })
     }
@@ -83,7 +42,12 @@ impl From<PerfHooksModule> for ModuleInfo<PerfHooksModule> {
 }
 
 pub fn init(ctx: &Ctx<'_>) -> Result<()> {
-    new_performance(ctx.clone())?;
+    let globals = ctx.globals();
+
+    let instance = Class::instance(ctx.clone(), Performance::new())?;
+    Performance::add_event_target_prototype(ctx)?;
+
+    globals.set("performance", instance)?;
     Ok(())
 }
 
