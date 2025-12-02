19ef41e73cd1546caee2240434d8f92d85d627f6
Richard Davison <ridaviso@amazon.com>
2025-12-02 09:42:15 +0100

    deduplicate symlink, use stable junction crate

--- modules/llrt_fs/src/symlink.rs
+++ modules/llrt_fs/src/symlink.rs
@@ -1,8 +1,39 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::io;
+
 use llrt_utils::result::ResultExt;
 use rquickjs::{function::Opt, Ctx, Result};
+
+fn symlink_blocking(target: &str, path: &str, type_value: Option<String>) -> io::Result<()> {
+    #[cfg(unix)]
+    {
+        _ = type_value;
+        std::os::unix::fs::symlink(target, path)
+    }
+    #[cfg(windows)]
+    {
+        let type_str = match type_value.as_deref() {
+            Some(t @ ("file" | "dir" | "junction")) => t,
+            _ => {
+                if std::fs::metadata(target)
+                    .map(|m| m.is_dir())
+                    .unwrap_or(false)
+                {
+                    "dir"
+                } else {
+                    "file"
+                }
+            },
+        };
+        match type_str {
+            "junction" => junction::create(target, path),
+            "dir" => std::os::windows::fs::symlink_dir(target, path),
+            _ => std::os::windows::fs::symlink_file(target, path),
+        }
+    }
+}
 
 pub async fn symlink<'js>(
     ctx: Ctx<'js>,
@@ -10,53 +41,12 @@ pub async fn symlink<'js>(
     path: String,
     type_value: Opt<String>,
 ) -> Result<()> {
-    let error_message = &["Can't create symlink \"", &path, "\""].concat();
+    let path_clone = path.clone();
 
-    #[cfg(unix)]
-    {
-        _ = type_value;
-        tokio::fs::symlink(&target, &path)
-            .await
-            .or_throw_msg(&ctx, error_message)?;
-    }
-    #[cfg(not(unix))]
-    {
-        let mut type_str = type_value.0.unwrap_or("".to_string());
-        if type_str != "file" && type_str != "dir" && type_str != "junction" && type_str != "" {
-            type_str = "".to_string();
-        }
-        if type_str == "" {
-            let metadata = tokio::fs::metadata(&target)
-                .await
-                .or_throw_msg(&ctx, &["Can't stat \"", &path, "\""].concat());
-            match metadata {
-                Ok(meta) => {
-                    if meta.is_dir() {
-                        type_str = "dir".to_string();
-                    } else {
-                        type_str = "file".to_string();
-                    }
-                },
-                Err(_) => {
-                    type_str = "file".to_string(); // Default to file if we can't determine
-                },
-            }
-        }
-        if type_str == "junction" {
-            std::os::windows::fs::junction_point(&target, &path)
-                .or_throw_msg(&ctx, error_message)?;
-        } else if type_str == "dir" {
-            tokio::fs::symlink_dir(&target, &path)
-                .await
-                .or_throw_msg(&ctx, error_message)?;
-        } else {
-            tokio::fs::symlink_file(&target, &path)
-                .await
-                .or_throw_msg(&ctx, error_message)?;
-        }
-    }
-
-    Ok(())
+    tokio::task::spawn_blocking(move || symlink_blocking(&target, &path_clone, type_value.0))
+        .await
+        .map_err(io::Error::other)?
+        .or_throw_msg(&ctx, &["Can't create symlink \"", &path, "\""].concat())
 }
 
 pub fn symlink_sync<'js>(
@@ -65,45 +55,6 @@ pub fn symlink_sync<'js>(
     path: String,
     type_value: Opt<String>,
 ) -> Result<()> {
-    let error_message = &["Can't create symlink \"", &path, "\""].concat();
-
-    #[cfg(unix)]
-    {
-        _ = type_value;
-        std::os::unix::fs::symlink(&target, &path).or_throw_msg(&ctx, error_message)?;
-    }
-    #[cfg(not(unix))]
-    {
-        use std::os::windows::fs;
-
-        let mut type_str = type_value.0.unwrap_or("".to_string());
-        if type_str != "file" && type_str != "dir" && type_str != "junction" && type_str != "" {
-            type_str = "".to_string();
-        }
-        if type_str == "" {
-            let metadata =
-                fs::metadata(&target).or_throw_msg(&ctx, &["Can't stat \"", &path, "\""].concat());
-            match metadata {
-                Ok(meta) => {
-                    if meta.is_dir() {
-                        type_str = "dir".to_string();
-                    } else {
-                        type_str = "file".to_string();
-                    }
-                },
-                Err(_) => {
-                    type_str = "file".to_string(); // Default to file if we can't determine
-                },
-            }
-        }
-        if type_str == "junction" {
-            fs::junction_point(&target, &path).or_throw_msg(&ctx, error_message)?;
-        } else if type_str == "dir" {
-            fs::symlink_dir(&target, &path).or_throw_msg(&ctx, error_message)?;
-        } else {
-            fs::symlink_file(&target, &path).or_throw_msg(&ctx, error_message)?;
-        }
-    }
-
-    Ok(())
+    symlink_blocking(&target, &path, type_value.0)
+        .or_throw_msg(&ctx, &["Can't create symlink \"", &path, "\""].concat())
 }
