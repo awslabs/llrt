ce0d72a29b2b52c0680a9335cafc9dbaadcea708
Richard Davison <ridaviso@amazon.com>
2025-12-19 23:13:24 +0100

    Gates

--- modules/llrt_fetch/src/fetch.rs
+++ modules/llrt_fetch/src/fetch.rs
@@ -483,10 +483,18 @@ fn get_option<'js, V: FromJs<'js> + Sized>(
 mod tests {
     use std::io::Read;
 
-    #[cfg(any(feature = "tls-ring", feature = "tls-aws-lc", feature = "tls-graviola"))]
+    #[cfg(any(
+        feature = "tls-ring",
+        feature = "tls-aws-lc",
+        all(feature = "tls-graviola", target_arch = "x86_64")
+    ))]
     use llrt_http::HttpsModule;
     use llrt_test::test_async_with;
-    #[cfg(any(feature = "tls-ring", feature = "tls-aws-lc", feature = "tls-graviola"))]
+    #[cfg(any(
+        feature = "tls-ring",
+        feature = "tls-aws-lc",
+        all(feature = "tls-graviola", target_arch = "x86_64")
+    ))]
     use llrt_test::{call_test, ModuleEvaluator};
     use rquickjs::{prelude::Promise, CatchResultExt};
     use wiremock::{matchers, Mock, MockServer, ResponseTemplate};
