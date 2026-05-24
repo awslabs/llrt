2fec2bee3f8773a1ab525053fc89caf746d4d29c
Shinya Watanabe <nabetti1720@gmail.com>
2026-05-24 22:21:40 +0900

    refactor

--- llrt_core/benches/encode_to_base64.rs
+++ llrt_core/benches/encode_to_base64.rs
@@ -1,11 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+mod utils;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 use llrt_encoding::*;
 use rquickjs::{Context, Runtime};
 
+use utils::random_ascii;
+
 fn criterion_benchmark(c: &mut Criterion) {
-    let seed = "hello world this is a longer buffer example for iteration testing, hello world this is a longer buffer example for iteration testing";
+    let seed = random_ascii(128);
 
     let runtime = Runtime::new().unwrap();
     runtime.set_max_stack_size(512 * 1024);
@@ -15,23 +19,24 @@ fn criterion_benchmark(c: &mut Criterion) {
     let _ =
         ctx.with(|ctx| ctx.eval::<String, &[u8]>(b"import {encodeToBase64} from 'llrt:codec';"));
 
-    let func = ["const seed = Buffer.from('", seed, "';"].concat();
+    let func = ["const seed = Buffer.from('", &seed, "');"].concat();
     let _ = ctx.with(|ctx| ctx.eval::<String, &[u8]>(func.as_bytes()));
 
-    c.bench_function("[JS] Buffer::from(bytes).toString('base64')", |b| {
-        let func = ["Buffer.from('", seed, "').toString('base64');"].concat();
-        ctx.with(|ctx| b.iter(|| ctx.eval::<String, &[u8]>(func.as_bytes())));
+    c.bench_function("[JS] Buffer::from(seed).toString('base64')", |b| {
+        ctx.with(|ctx| {
+            b.iter(|| ctx.eval::<String, &[u8]>(b"Buffer.from(seed).toString('base64')"))
+        });
     });
 
-    c.bench_function("[JS] Uint8Array.toBase64()", |b| {
+    c.bench_function("[JS] Uint8Array.toBase64(seed)", |b| {
         ctx.with(|ctx| b.iter(|| ctx.eval::<String, &[u8]>(b"seed.toBase64()")));
     });
 
-    c.bench_function("[JS] llrt:codec.encodeToBase64()", |b| {
+    c.bench_function("[JS] llrt:codec.encodeToBase64(seed)", |b| {
         ctx.with(|ctx| b.iter(|| ctx.eval::<String, &[u8]>(b"encodeToBase64(seed)")));
     });
 
-    c.bench_function("[Native] llrt_encoding::bytes_to_b64()", |b| {
+    c.bench_function("[Native] llrt_encoding::bytes_to_b64(seed)", |b| {
         b.iter(|| bytes_to_b64(seed.as_bytes()))
     });
 }
--- llrt_core/benches/encode_to_hex.rs
+++ llrt_core/benches/encode_to_hex.rs
@@ -1,11 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+mod utils;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 use llrt_encoding::*;
 use rquickjs::{Context, Runtime};
 
+use utils::random_ascii;
+
 fn criterion_benchmark(c: &mut Criterion) {
-    let seed = "hello world this is a longer buffer example for iteration testing, hello world this is a longer buffer example for iteration testing";
+    let seed = random_ascii(128);
 
     let runtime = Runtime::new().unwrap();
     runtime.set_max_stack_size(512 * 1024);
@@ -14,23 +18,22 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     let _ = ctx.with(|ctx| ctx.eval::<String, &[u8]>(b"import {encodeToHex} from 'llrt:codec';"));
 
-    let func = ["const seed = Buffer.from('", seed, "';"].concat();
+    let func = ["const seed = Buffer.from('", &seed, "');"].concat();
     let _ = ctx.with(|ctx| ctx.eval::<String, &[u8]>(func.as_bytes()));
 
-    c.bench_function("[JS] Buffer::from(bytes).toString('hex')", |b| {
-        let func = ["Buffer.from('", seed, "').toString('hex');"].concat();
-        ctx.with(|ctx| b.iter(|| ctx.eval::<String, &[u8]>(func.as_bytes())));
+    c.bench_function("[JS] Buffer::from(seed).toString('hex')", |b| {
+        ctx.with(|ctx| b.iter(|| ctx.eval::<String, &[u8]>(b"Buffer.from(seed).toString('hex')")));
     });
 
-    c.bench_function("[JS] Uint8Array.toHex()", |b| {
+    c.bench_function("[JS] Uint8Array.toHex(seed)", |b| {
         ctx.with(|ctx| b.iter(|| ctx.eval::<String, &[u8]>(b"seed.toHex()")));
     });
 
-    c.bench_function("[JS] llrt:codec.encodeToHex()", |b| {
+    c.bench_function("[JS] llrt:codec.encodeToHex(seed)", |b| {
         ctx.with(|ctx| b.iter(|| ctx.eval::<String, &[u8]>(b"encodeToHex(seed)")));
     });
 
-    c.bench_function("[Native] llrt_encoding::bytes_to_hex()", |b| {
+    c.bench_function("[Native] llrt_encoding::bytes_to_hex(seed)", |b| {
         b.iter(|| bytes_to_hex(seed.as_bytes()))
     });
 }
