2fbe9f30af6f476a55197c0db4938d031978ce67
Shinya Watanabe <nabetti1720@gmail.com>
2026-09-12 12:23:57 +0900

    chore(wpt): Skip browser-only WebCrypto historical WPT in LLRT

--- tests/wpt/WebCryptoAPI.test.ts
+++ tests/wpt/WebCryptoAPI.test.ts
@@ -1,5 +1,6 @@
 import { runSuite, runTestDynamic } from "./WebCryptoAPI.harness.js";
 
 runSuite(import.meta.url, runTestDynamic, [
+  "historical.any.js", // Non-secure Window/Worker context is not applicable to LLRT
   "idlharness.https.any.js", // ReferenceError: idl_test is not defined
 ]);
